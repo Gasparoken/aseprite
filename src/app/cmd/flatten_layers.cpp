c63f82b2c339696bd2e19eda8c40224e4f7480e6
Gaspar Capello <gaspar@igarastudio.com>
2019-10-17 10:37:33 -0300

    Fix Flatten Visible crash when no layers are visible
    Bug founded by Jerry Pie from Steam
    https://steamcommunity.com/app/431730/discussions/0/1606022547906751802/

--- src/app/cmd/flatten_layers.cpp
+++ src/app/cmd/flatten_layers.cpp
@@ -61,85 +61,87 @@ void FlattenLayers::onExecute()
       backgroundIsSel = true;
   }
 
-  // Create a temporary image.
-  ImageRef image(Image::create(sprite->pixelFormat(),
-      sprite->width(),
-      sprite->height()));
+  LayerList list = layers.toLayerList();
+  if (list.size() > 0) {
+    // Create a temporary image.
+    ImageRef image(Image::create(sprite->pixelFormat(),
+        sprite->width(),
+        sprite->height()));
 
-  LayerImage* flatLayer;  // The layer onto which everything will be flattened.
-  color_t     bgcolor;    // The background color to use for flatLayer.
+    LayerImage* flatLayer;  // The layer onto which everything will be flattened.
+    color_t     bgcolor;    // The background color to use for flatLayer.
 
-  flatLayer = sprite->backgroundLayer();
-  if (backgroundIsSel && flatLayer && flatLayer->isVisible()) {
-    // There exists a visible background layer, so we will flatten onto that.
-    bgcolor = doc->bgColor(flatLayer);
-  }
-  else {
-    // Create a new transparent layer to flatten everything onto it.
-    flatLayer = new LayerImage(sprite);
-    ASSERT(flatLayer->isVisible());
-    executeAndAdd(new cmd::AddLayer(sprite->root(), flatLayer, nullptr));
-    executeAndAdd(new cmd::SetLayerName(flatLayer, "Flattened"));
-    bgcolor = sprite->transparentColor();
+    flatLayer = sprite->backgroundLayer();
+    if (backgroundIsSel && flatLayer && flatLayer->isVisible()) {
+      // There exists a visible background layer, so we will flatten onto that.
+      bgcolor = doc->bgColor(flatLayer);
+    }
+    else {
+      // Create a new transparent layer to flatten everything onto it.
+      flatLayer = new LayerImage(sprite);
+      ASSERT(flatLayer->isVisible());
+      executeAndAdd(new cmd::AddLayer(sprite->root(), flatLayer, nullptr));
+      executeAndAdd(new cmd::SetLayerName(flatLayer, "Flattened"));
+      bgcolor = sprite->transparentColor();
 
-    LayerList list = layers.toLayerList();
-    if (list.front())
-      executeAndAdd(new cmd::MoveLayer(flatLayer,
-                                       list.front()->parent(),
-                                       list.front()));
-  }
+      if (list.front())
+        executeAndAdd(new cmd::MoveLayer(flatLayer,
+                                        list.front()->parent(),
+                                        list.front()));
+    }
 
-  render::Render render;
-  render.setNewBlend(m_newBlendMethod);
-  render.setBgType(render::BgType::NONE);
+    render::Render render;
+    render.setNewBlend(m_newBlendMethod);
+    render.setBgType(render::BgType::NONE);
 
-  {
-    // Show only the layers to be flattened so other layers are hidden
-    // temporarily.
-    RestoreVisibleLayers restore;
-    restore.showSelectedLayers(sprite, layers);
+    {
+      // Show only the layers to be flattened so other layers are hidden
+      // temporarily.
+      RestoreVisibleLayers restore;
+      restore.showSelectedLayers(sprite, layers);
 
-    // Copy all frames to the background.
-    for (frame_t frame(0); frame<sprite->totalFrames(); ++frame) {
-      // Clear the image and render this frame.
-      clear_image(image.get(), bgcolor);
-      render.renderSprite(image.get(), sprite, frame);
+      // Copy all frames to the background.
+      for (frame_t frame(0); frame<sprite->totalFrames(); ++frame) {
+        // Clear the image and render this frame.
+        clear_image(image.get(), bgcolor);
+        render.renderSprite(image.get(), sprite, frame);
 
-      // TODO Keep cel links when possible
+        // TODO Keep cel links when possible
 
-      ImageRef cel_image;
-      Cel* cel = flatLayer->cel(frame);
-      if (cel) {
-        if (cel->links())
-          executeAndAdd(new cmd::UnlinkCel(cel));
+        ImageRef cel_image;
+        Cel* cel = flatLayer->cel(frame);
+        if (cel) {
+          if (cel->links())
+            executeAndAdd(new cmd::UnlinkCel(cel));
 
-        cel_image = cel->imageRef();
-        ASSERT(cel_image);
+          cel_image = cel->imageRef();
+          ASSERT(cel_image);
 
-        executeAndAdd(
-          new cmd::CopyRect(cel_image.get(), image.get(),
-                            gfx::Clip(0, 0, image->bounds())));
-      }
-      else {
-        gfx::Rect bounds(image->bounds());
-        if (doc::algorithm::shrink_bounds(
-              image.get(), bounds, image->maskColor())) {
-          cel_image.reset(
-            doc::crop_image(image.get(), bounds, image->maskColor()));
-          cel = new Cel(frame, cel_image);
-          cel->setPosition(bounds.origin());
-          flatLayer->addCel(cel);
+          executeAndAdd(
+            new cmd::CopyRect(cel_image.get(), image.get(),
+                              gfx::Clip(0, 0, image->bounds())));
+        }
+        else {
+          gfx::Rect bounds(image->bounds());
+          if (doc::algorithm::shrink_bounds(
+                image.get(), bounds, image->maskColor())) {
+            cel_image.reset(
+              doc::crop_image(image.get(), bounds, image->maskColor()));
+            cel = new Cel(frame, cel_image);
+            cel->setPosition(bounds.origin());
+            flatLayer->addCel(cel);
+          }
         }
       }
     }
-  }
 
-  // Delete flattened layers.
-  for (Layer* layer : layers) {
-    // layer can be == flatLayer when we are flattening on the
-    // background layer.
-    if (layer != flatLayer) {
-      executeAndAdd(new cmd::RemoveLayer(layer));
+    // Delete flattened layers.
+    for (Layer* layer : layers) {
+      // layer can be == flatLayer when we are flattening on the
+      // background layer.
+      if (layer != flatLayer) {
+        executeAndAdd(new cmd::RemoveLayer(layer));
+      }
     }
   }
 }
